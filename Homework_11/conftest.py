71b5b5bf9bad7d15f2cb7e10cdbea2952f09727b
Anastasiya Kuznetsova <forquest@icloud.com>
2025-03-08 22:46:14 +0300

    added webdriver

--- Homework_11/conftest.py
+++ Homework_11/conftest.py
@@ -1,26 +1,26 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selene import browser
+from selene import Browser, Config
 
 @pytest.fixture(scope="function")
 def setup_browser():
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
-        "browserVersion": "120.0",
+        "browserVersion": "100.0",
         "selenoid:options": {
             "enableVNC": True,
             "enableVideo": True
         }
     }
-    options.set_capability("selenoid:options", selenoid_capabilities)
-
+    options.capabilities.update(selenoid_capabilities)
     driver = webdriver.Remote(
         command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
         options=options
     )
 
-    browser.config.driver = driver
+    browser = Browser(Config(driver))
+    yield browser
 
     driver.quit()
