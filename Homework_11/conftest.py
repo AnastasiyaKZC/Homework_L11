b163cd50209ca1060f92f768467172c41aaf2bc8
Anastasiya Kuznetsova <forquest@icloud.com>
2025-03-08 21:49:47 +0300

    added demoqa_test

--- Homework_11/conftest.py
+++ Homework_11/conftest.py
@@ -1,12 +1,34 @@
 import pytest
-from selene import browser
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selene import browser, Config, Browser
 
-@pytest.fixture(scope="function", autouse=True)
-def browser_managment():
-    browser.config.window_width = 1280
-    browser.config.window_height = 1220
-    # browser.config.base_url = "https://demoqa.com/automation-practice-form"
-    browser.config.timeout = 2.0
 
-    yield
-    browser.quit()
+@pytest.fixture(scope="function")
+def setup_browser():
+   options = Options()
+   selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+   options.capabilities.update(selenoid_capabilities)
+   driver = webdriver.Remote(
+        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options)
+
+   browser = Browser(Config(driver))
+   yield browser
+
+   browser.quit()
+
+    # browser.config.window_width = 1280
+    # browser.config.window_height = 1220
+    # # browser.config.base_url = "https://demoqa.com/automation-practice-form"
+    # browser.config.timeout = 2.0
+    #
+    # yield
+    # browser.quit()
--- Homework_11/test_form_submission.py
+++ Homework_11/test_form_submission.py
@@ -1,49 +1,50 @@
+import allure
 from selene import browser, have, be, command
 
-
+@allure.title("Успешное заполнение формы регистрации")
 def test_fill_form():
-    # Заполняю личные данные
-    browser.open("https://demoqa.com/automation-practice-form")
-    browser.element("#firstName").type("Анастасия")
-    browser.element("#lastName").type("Кузнецова")
-    browser.element("#userEmail").type("kuznetsova@mail.com")
+    with allure.step("открываю браузер"):
+        browser.open("https://demoqa.com/automation-practice-form")
+        browser.element("#firstName").type("Анастасия")
+        browser.element("#lastName").type("Кузнецова")
+        browser.element("#userEmail").type("kuznetsova@mail.com")
 
-    # Выбираю пол
-    browser.element('[for="gender-radio-2"]').click()
+    with allure.step("выбираю пол"):
+        browser.element('[for="gender-radio-2"]').click()
 
-    # Ввожу номер телефона
-    browser.element("#userNumber").type("1234567890")
+    with allure.step("ввожу номер телефона"):
+        browser.element("#userNumber").type("1234567890")
 
-    # Выбираю дату рождения
-    browser.element("#dateOfBirthInput").click()
-    browser.element(".react-datepicker__year-select").type("1984")
-    browser.element(".react-datepicker__month-select").type("May")
-    browser.element(".react-datepicker__day--009").click()
+    with allure.step("выбираю дату рождения"):
+        browser.element("#dateOfBirthInput").click()
+        browser.element(".react-datepicker__year-select").type("1984")
+        browser.element(".react-datepicker__month-select").type("May")
+        browser.element(".react-datepicker__day--009").click()
 
-    # Ввожу предметы
-    browser.element("#subjectsInput").type("Math").press_enter() #тут повозилась - не знала, что это выпадашка, пыталась ввести свой текст
+    with allure.step("ввожу предметы"):
+        browser.element("#subjectsInput").type("Math").press_enter() #тут повозилась - не знала, что это выпадашка, пыталась ввести свой текст
 
-    # Выбираю хобби
-    browser.element('[for="hobbies-checkbox-2"]').click()
+    with allure.step("выбираю хобби"):
+        browser.element('[for="hobbies-checkbox-2"]').click()
 
-    # Загружаю изображение
-    browser.element("#uploadPicture").send_keys("/Users/kuznetsova/Desktop/download.jpg")
+    with allure.step("загружаю изображение"):
+        browser.element("#uploadPicture").send_keys("/Users/kuznetsova/Desktop/download.jpg")
 
-    # Вводим текущий адрес
-    browser.element("#currentAddress").type("Ростов-на-Дону, ул.Города Волос")
+    with allure.step("ввожу адрес"):
+        browser.element("#currentAddress").type("Ростов-на-Дону, ул.Города Волос")
 
-    # Выбираем штат
-    browser.element("#state").perform(command.js.scroll_into_view).click() # скролл к элементу
-    browser.element("#state").should(be.clickable).click() # задержка до появления списка
-    browser.element("#state").click()
-    browser.all("div.css-11unzgr").element_by(have.text("Haryana")).click()
+    with allure.step("выбираю штат"):
+        browser.element("#state").perform(command.js.scroll_into_view).click() # скролл к элементу
+        browser.element("#state").should(be.clickable).click() # задержка до появления списка
+        browser.element("#state").click()
+        browser.all("div.css-11unzgr").element_by(have.text("Haryana")).click()
 
-    # Дожидаемся загрузки списка городов
-    browser.element("#city").should(be.clickable).click() # задержка до появления списка
-    browser.all("div.css-11unzgr").element_by(have.text("Karnal")).click()
+    with allure.step("дожидаюсь загрузки списка городов"):
+        browser.element("#city").should(be.clickable).click() # задержка до появления списка
+        browser.all("div.css-11unzgr").element_by(have.text("Karnal")).click()
 
-    # Отправляем форму
-    browser.element("#submit").press_enter()
+    with allure.step("отправляю форму"):
+        browser.element("#submit").press_enter()
 
-    # Проверяем, что форма отправлена (например, через проверку заголовка или успешного сообщения)
-    browser.element(".modal-title").should(have.text("Thanks for submitting the form"))
+    with allure.step("проверка что форма отправлена"):
+        browser.element(".modal-title").should(have.text("Thanks for submitting the form"))
