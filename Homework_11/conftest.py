a0d9ff66a5b9c9491248110e0e2d95f1120f8874
Anastasiya Kuznetsova <forquest@icloud.com>
2025-03-08 22:07:06 +0300

    added demoqa_test

--- Homework_11/conftest.py
+++ Homework_11/conftest.py
@@ -1,13 +1,13 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selene import browser, Config, Browser
+from selene import browser
 
 
 @pytest.fixture(scope="function")
 def setup_browser():
-   options = Options()
-   selenoid_capabilities = {
+    options = Options()
+    selenoid_capabilities = {
         "browserName": "chrome",
         "browserVersion": "100.0",
         "selenoid:options": {
@@ -15,20 +15,19 @@ def setup_browser():
             "enableVideo": True
         }
     }
-   options.capabilities.update(selenoid_capabilities)
-   driver = webdriver.Remote(
+    options.capabilities.update(selenoid_capabilities)
+
+    driver = webdriver.Remote(
         command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
-        options=options)
+        options=options
+    )
 
-   browser = Browser(Config(driver))
-   yield browser
+    # Привязываем WebDriver к Selene
+    browser.config.driver = driver
+    browser.config.window_width = 1280
+    browser.config.window_height = 1220
+    browser.config.timeout = 2.0
 
-   browser.quit()
+    yield browser  # Передаём в тест настроенный browser
 
-    # browser.config.window_width = 1280
-    # browser.config.window_height = 1220
-    # # browser.config.base_url = "https://demoqa.com/automation-practice-form"
-    # browser.config.timeout = 2.0
-    #
-    # yield
-    # browser.quit()
+    driver.quit()  # Закрываем WebDriver
